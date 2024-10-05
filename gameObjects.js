3591c8e95aab71b4871cc6340fa04bab2a62f3df
Double A <abidfahim932@gmail.com>
2024-10-05 10:29:59 +0600

    Small change

--- gameObjects.js
+++ gameObjects.js
@@ -20,112 +20,128 @@ const pawn = [
         isTaken: false,
         positionX: 1,
         positionY: 0,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "white",
         isTaken: false,
         positionX: 1,
         positionY: 1,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "white",
         isTaken: false,
         positionX: 1,
         positionY: 2,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "white",
         isTaken: false,
         positionX: 1,
         positionY: 3,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "white",
         isTaken: false,
         positionX: 1,
         positionY: 4,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "white",
         isTaken: false,
         positionX: 1,
         positionY: 5,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "white",
         isTaken: false,
         positionX: 1,
         positionY: 6,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "white",
         isTaken: false,
         positionX: 1,
         positionY: 7,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 0,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 1,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 2,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 3,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 4,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 5,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 6,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
     {
         color: "black",
         isTaken: false,
         positionX: 6,
         positionY: 7,
-        id: document.getElementById('')
+        id: document.getElementById(''),
+        isFirstMove:  true
     },
 ]
 
@@ -282,3 +298,30 @@ queen.forEach(piece => {
 king.forEach(piece => {
     board[piece.positionX][piece.positionY].whoOccupied = piece
 })
+
+
+pawn[0].id.addEventListener("click", () => readyToMovePawn(pawn[0]))
+pawn[1].id.addEventListener("click", () => readyToMovePawn(pawn[1]))
+pawn[2].id.addEventListener("click", () => readyToMovePawn(pawn[2]))
+pawn[3].id.addEventListener("click", () => readyToMovePawn(pawn[3]))
+pawn[4].id.addEventListener("click", () => readyToMovePawn(pawn[4]))
+pawn[5].id.addEventListener("click", () => readyToMovePawn(pawn[5]))
+pawn[6].id.addEventListener("click", () => readyToMovePawn(pawn[6]))
+pawn[7].id.addEventListener("click", () => readyToMovePawn(pawn[7]))
+pawn[8].id.addEventListener("click", () => readyToMovePawn(pawn[8]))
+pawn[9].id.addEventListener("click", () => readyToMovePawn(pawn[9]))
+pawn[10].id.addEventListener("click", () => readyToMovePawn(pawn[10]))
+pawn[11].id.addEventListener("click", () => readyToMovePawn(pawn[11]))
+pawn[12].id.addEventListener("click", () => readyToMovePawn(pawn[12]))
+pawn[13].id.addEventListener("click", () => readyToMovePawn(pawn[13]))
+pawn[14].id.addEventListener("click", () => readyToMovePawn(pawn[14]))
+pawn[15].id.addEventListener("click", () => readyToMovePawn(pawn[15]))
+
+function readyToMovePawn(pawnPiece) {
+    if(pawnPiece.isFirstMove) {
+        if(board[pawnPiece.positionX + 1][pawnPiece.positionY].whoOccupied === false && board[pawnPiece.positionX + 2][pawnPiece.positionY] === false) {
+            board[pawnPiece.positionX + 1][pawnPiece.positionY].style.brightness = "1.5"
+            board[pawnPiece.positionX + 2][pawnPiece.positionY].style.brightness = "1.5"
+        }
+    }
+}
